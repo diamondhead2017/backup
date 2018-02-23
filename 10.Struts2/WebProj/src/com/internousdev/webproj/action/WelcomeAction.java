b54afb19665357ace668fae7807849d170619bd4
YamadaToshiki <dcm.sb23@gmail.com>
2018-02-23 21:41:11 +0900

    2/23

--- 10.Struts2/WebProj/src/com/internousdev/webproj/action/WelcomeAction.java
+++ 10.Struts2/WebProj/src/com/internousdev/webproj/action/WelcomeAction.java
@@ -1,10 +1,9 @@
 package com.internousdev.webproj.action;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class WelcomeAction {
-
-  public String execute(){
-	  return SUCCESS;
-  }
-
+public class WelcomeAction extends ActionSupport{
+	public String excute(){
+		return SUCCESS;
+	}
 }
